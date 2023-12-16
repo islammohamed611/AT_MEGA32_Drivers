4e9da3f8ad3c079fd55d0167645d2c8d01e14525
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-16 21:26:40 +0200

    9_STEPPER_MOTOR

---
+++ COTS/2-HAL/9_STEPPER_MOTOR/STEPPER_private.h
@@ -0,0 +1,15 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    STEPPER_private.c    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *
+ *  Author : Islam Mohamed Abdullah
+ *  Layer  : HAL
+ *  SWC    : STEPPER_MOTOR
+ *
+ */
+
+#ifndef HAL_STEPPER_MOTOR_STEPPER_PRIVATE_H_
+#define HAL_STEPPER_MOTOR_STEPPER_PRIVATE_H_
+
+
+
+#endif /* HAL_STEPPER_MOTOR_STEPPER_PRIVATE_H_ */
