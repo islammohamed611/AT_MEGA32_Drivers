3b910b8756ea85cf3ea50d08e12de8497a67b6b4
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-16 21:07:36 +0200

    7_LM35

---
+++ COTS/2-HAL/7_LM35/LM35_config.h
@@ -0,0 +1,14 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    LM35_config.h    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *
+ *  Author : Islam Mohamed Abdullah 
+ *  Layer  : HAL
+ *  SWC    : LM35
+ *
+ */
+#ifndef LM35_CONFIG_H_
+#define LM35_CONFIG_H_
+
+	
+
+#endif
---
+++ COTS/2-HAL/7_LM35/LM35_private.h
@@ -0,0 +1,14 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    LM35_private.h    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *
+ *  Author :Islam Mohamed Abdullah 
+ *  Layer  : HAL
+ *  SWC    : LM35
+ *
+ */
+#ifndef LM35_PRIVATE_H_
+#define LM35_PRIVATE_H_
+
+	
+
+#endif
